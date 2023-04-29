2aceab8c07ca6b9db3d813f9e55f6fd7a0692dba
real wwwdev <sheen3d@yandex.ru>
2023-04-29 20:31:45 +0500

    adding js file with questions

---
+++ questions.js
@@ -0,0 +1,61 @@
+// создание массива вопросов с вариантами ответов
+let questions = [
+  {
+    numb: 1,
+    question: 'Что такое doctype?',
+    answer: 'Document Type Declaration',
+    options: [
+      'Document Type Declaration',
+      'Document Type Expression',
+      'Document Type Element',
+      'Document Type Web',
+    ],
+  },
+  {
+    numb: 2,
+    question: 'Что такое DOM?',
+    answer: 'Document Object Model',
+    options: [
+      'Document Output Model',
+      'Document Operator Method',
+      'Document Object Model',
+      'Document Object Method',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'Что означает this в Javascript?',
+    answer: 'Контекст вызова',
+    options: ['Контекст объекта', 'Контекст переменной', 'Контекст вызова', 'Контекст массива'],
+  },
+  {
+    numb: 4,
+    question: 'Объектами какого порядка являются функции в JS?',
+    answer: 'Первого порядка',
+    options: ['Первого порядка', 'Второго порядка', 'Третьего порядка', 'Четвертого порядка'],
+  },
+  {
+    numb: 5,
+    question: 'Что такое IIFE?',
+    answer: 'Immediately Invoked Function Expression',
+    options: [
+      'Immediately Inline Function Element',
+      'Introduction Include Form Element',
+      'Immediately Include Function Event',
+      'Immediately Invoked Function Expression',
+    ],
+  },
+  // Раскомментируй код, чтобы добавить вопрос
+
+  //   {
+  //   numb: 6,
+  //   question: "Вопрос",
+  //   answer: "Правильный ответ",
+  //   options: [
+  //     "Вариант 1",
+  //     "Вариант 2",
+  //     "Вариант 3",
+  //     "Вариант 4"
+  //   ]
+  // },
+];
